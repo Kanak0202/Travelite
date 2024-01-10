9511dc5026043fef853035df190c04b7a72a1b06
Kanak0202 <kanakkhandelwal0202@gmail.com>
2024-01-10 16:11:46 +0530

    Explore page

--- server/routes/route.js
+++ server/routes/route.js
@@ -2,12 +2,13 @@ import express from "express";
 
 //controllers
 import { signup, login } from "../controller/auth-controller.js";
-import { add } from "../controller/destination-review-controller.js";
+import { add, getUniquePlaces } from "../controller/destination-review-controller.js";
 
 const route = express.Router();
 
 route.post("/signup", signup);
 route.post("/login", login);
 route.post("/add", add);
+route.get("/explore", getUniquePlaces);
 
 export default route;
