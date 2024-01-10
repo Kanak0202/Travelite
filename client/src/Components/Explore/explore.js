8a68c8ef7e82e13f5ae365d4b4299adda906c57f
Kanak0202 <kanakkhandelwal0202@gmail.com>
2024-01-11 01:19:02 +0530

    Search functionality for explore

--- client/src/Components/Explore/explore.js
+++ client/src/Components/Explore/explore.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import emptiness from "./img/emptmain.png";
 import background from "./img/background.jpg";
-import loadPlane from "./img/loadPlane.gif"; // Adjust the path accordingly
+import loadPlane from "./img/loadPlane.gif";
+import balloons1 from "./img/balloons1.jpg";
 import "./explore.css";
 
 const Explore = () => {
   const [places, setPlaces] = useState([]);
   const [placeWithPhoto, setPlaceWithPhoto] = useState({});
   const [loading, setLoading] = useState(true);
+  const [key, setKey] = useState("");
+  const [searchPlaces, setSearchPlaces] = useState([]);
 
   const retrievePlaceNames = async () => {
     try {
@@ -37,12 +40,12 @@ const Explore = () => {
     }
   };
 
-  const getBackgroundPhotos = async () => {
+  const getBackgroundPhotos = async (placesArray) => {
     const photoMap = {};
 
-    for (const place of places) {
+    for (const place of placesArray) {
       const photo = await getPhoto(place);
-      const photoWithOverlay = `linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.3)), url(${photo})`;
+      const photoWithOverlay = `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${photo})`;
       photoMap[place] = photoWithOverlay;
     }
 
@@ -57,32 +60,52 @@ const Explore = () => {
   useEffect(() => {
     if (places.length > 0) {
       setLoading(true); // Set loading to true when new places are fetched
-      getBackgroundPhotos();
+      getBackgroundPhotos(places);
     }
   }, [places]);
 
+  const searchDestination = async(e)=>{
+    let searchKey = e.target.value;
+    if(searchKey){
+      console.log(searchKey);
+      setKey(searchKey);
+      let result = await fetch(`http://localhost:8000/search/${searchKey}`);
+      result = await result.json();
+      setSearchPlaces(result);
+    } else if(searchKey === ""){
+      setKey("");
+      setSearchPlaces([]);
+    }
+  }
+
   return (
     <div>
+      <div className="search-container" style={{ backgroundImage: `url(${balloons1})`, backgroundSize: 'cover' }}>
+        <input type="text" className="search-input-box" placeholder="I want to visit..." onChange={searchDestination}/>
+        <div className="search-para">
+          {(key && key.trim() !== "") ? <p>Results for {key}...</p> : null}
+        </div>
+      </div>
       {loading && (
         <div className="loading-container">
-          {/* <p>Loading...</p> */}
           <img className="loading-img" src={loadPlane} alt="Loading..." />
         </div>
       )}
-      {!loading && places.length === 0 && (
-        <div className="no-result-container">
-          <img className="no-result-img" src={emptiness} alt="No results to display" />
-        </div>
-      )}
-      {!loading && places.length > 0 && (
-        <div className="all-places-container">
-          {places.map((place, index) => (
-            <div className="place-container" key={index} style={{ backgroundImage: placeWithPhoto[place], backgroundSize: 'cover' }}>
-              <p className="place-name">{place}</p>
-            </div>
-          ))}
-        </div>
-      )}
+      {!loading && (searchPlaces.length === 0 && key !== "") && (
+  <div className="no-result-container">
+    <img className="no-result-img" src={emptiness} alt="No results to display" />
+  </div>
+)}
+
+{!loading && (searchPlaces.length > 0 || (key === "" && places.length > 0)) && (
+  <div className="all-places-container">
+    {(searchPlaces.length > 0 ? searchPlaces : places).map((place, index) => (
+      <div className="place-container" key={index} style={{ backgroundImage: placeWithPhoto[place], backgroundSize: 'cover' }}>
+        <p className="place-name">{place}</p>
+      </div>
+    ))}
+  </div>
+)}
     </div>
   );
 };
--- server/routes/route.js
+++ server/routes/route.js
@@ -2,7 +2,7 @@ import express from "express";
 
 //controllers
 import { signup, login } from "../controller/auth-controller.js";
-import { add, getUniquePlaces } from "../controller/destination-review-controller.js";
+import { add, getUniquePlaces,searchPlace } from "../controller/destination-review-controller.js";
 
 const route = express.Router();
 
@@ -10,5 +10,6 @@ route.post("/signup", signup);
 route.post("/login", login);
 route.post("/add", add);
 route.get("/explore", getUniquePlaces);
+route.get("/search/:key", searchPlace);
 
 export default route;
