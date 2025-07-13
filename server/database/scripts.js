d3700f89ce38e51ec930bddbf34df14ab9c04f01
Kanak0202 <kanakkhandelwal0202@gmail.com>
2025-07-13 10:41:46 +0530

    mongo script

---
+++ server/database/scripts.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import destination from "../models/DestinationReview.js";
+
+//model
+import place from "../models/Place.js";
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const uri = "uri";//add uri here
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+async function run() {
+  try {
+    // Example: Set all users' status to "active"
+    const result = await place.deleteMany({}, { name: "Jaipur" });
+    console.log("Deletion complete:", result);
+  } catch (err) {
+    console.error("Error:", err);
+  } finally {
+    mongoose.disconnect();
+  }
+}
+async function run2() {
+  try {
+    // Example: Set all users' status to "active"
+    const result = await destination.deleteMany({}, { place: "Jaipur" });
+    console.log("Deletion complete:", result);
+  } catch (err) {
+    console.error("Error:", err);
+  } finally {
+    mongoose.disconnect();
+  }
+}
+
+run2();
+
